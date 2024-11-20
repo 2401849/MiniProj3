99abb5f5ba1bb78476c324d94b7b5f0d9a595ed7
2401849 <2401849@estudante.uab.pt>
2024-11-20 20:05:42 +0000

    updated store

--- animalec/frontend/src/store/index.js
+++ animalec/frontend/src/store/index.js
@@ -4,6 +4,8 @@ import Vuex from "vuex";
 import moduleBase from "./module";
 import moduleAuth from "./auth/auth.module";
 import moduleUser from "./users/user.module";
+import moduleSponsor from "./sponsors/sponsor.module";
+import moduleExpert from "./experts/expert.module";
 import moduleAnimal from "./animals/animal.module";
 import moduleQuiz from "./quizzes/quiz.module";
 import moduleQuestion from "./questions/question.module";
@@ -19,7 +21,9 @@ export default new Vuex.Store({
     auth: moduleAuth,
     animal: moduleAnimal,
     user: moduleUser,
+    sponsor: moduleSponsor,
+    expert: moduleExpert,
     quiz: moduleQuiz,
-    question: moduleQuestion
-  }
+    question: moduleQuestion,
+  },
 });
