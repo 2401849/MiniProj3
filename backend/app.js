5d64f4db54383cd4480a58b4d6067ab42a1b0ec7
2401849 <2401849@estudante.uab.pt>
2024-12-17 16:58:54 +0000

    swagger intro

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,21 @@
 const port = process.env.PORT || 8080;
-const host = process.env.HOST || '0.0.0.0';
-const express = require('express');
+const host = process.env.HOST || "0.0.0.0";
+const express = require("express");
+const generator = require("express-oas-generator");
 const app = express();
+generator.handleResponses(app, {});
 
-app.use('/assets', express.static('assets'));
-app.use('/views', express.static('views'));
+app.use("/assets", express.static("assets"));
+app.use("/views", express.static("views"));
 
-
-require('./init/db.js')(app, () => {
-  require('./init/middleware')(app);
-  require('./init/router')(app);
+require("./init/db.js")(app, () => {
+  require("./init/middleware")(app);
+  require("./init/router")(app);
+  generator.handleRequests();
   app.listen(port, host, (error) => {
     if (error) throw error;
-    console.log('Your app is listening on ' + port);
+    console.log(
+      "Your app is listening on " + port + "\nApi Documentation at /api-doc"
+    );
   });
 });
