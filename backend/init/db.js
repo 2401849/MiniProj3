540bf7823af4ec5873b58b98fd82356570e42776
2401849 <2401849@estudante.uab.pt>
2024-12-17 17:01:19 +0000

    swagger intro

--- backend/init/db.js
+++ backend/init/db.js
@@ -1,18 +1,21 @@
 module.exports = (app, callback) => {
-    const CONFIG = require('../config/config');
-    //Connect to DB
-    const mongoose = require('mongoose');
-    let settings = {
-        reconnectTries: Number.MAX_VALUE,
+  const CONFIG = require("../config/config");
+  //Connect to DB
+  const mongoose = require("mongoose");
+  let settings = {
+    /*reconnectTries: Number.MAX_VALUE, (no longer necessary in newer versions) https://mongoosejs.com/docs/migrating_to_6.html#removed-reconnecttries-and-reconnectinterval-options
         autoReconnect: true,
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useFindAndModify: false
-    };
-    global.mongoConnection = mongoose.createConnection(CONFIG.mongodb.uri, settings, (error) => {
-        if (error) throw error;
-        console.log('---Connected to DB');
-        return callback();
-    })
-
-}
+        useFindAndModify: false*/
+  };
+  global.mongoConnection = mongoose.createConnection(
+    CONFIG.mongodb.uri,
+    settings,
+    (error) => {
+      if (error) throw error;
+      console.log("---Connected to DB");
+      return callback();
+    }
+  );
+};
